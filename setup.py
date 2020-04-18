cb32d8104b543c68e736de9ae792ee9baa5d13b4
Jean-Claude Passy <jean-claude.passy@tuebingen.mpg.de>
2020-04-18 18:57:08 +0200

    Added version for uploading package to PyPI.

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from setuptools import setup,find_packages
-import sys
-from os import path
+from pathlib import Path
 
-setup(name = "city_graph",
-      packages=find_packages('.'),
-      description="framework for representing a city and moving in it",
+from setuptools import setup, find_packages
+
+
+def _get_version():
+    """"Convenient function to get the version of this package."""
+
+    version_path = Path() / 'city_graph' / 'version.py'
+    if not version_path.exists:
+        return None
+    with open(version_path) as version_file:
+        ns = {}
+        exec(version_file.read(), ns)
+
+    return ns['__version__']
+
+
+setup(name="city_graph",
+      version=_get_version(),
+      packages=find_packages(),
+      description="Framework for representing a city and moving in it",
       url="https://github.com/MPI-IS/CityGraph.git",
       author="Jean Claude Passy, Ivan Oreshnikov, Vincent Berenz",
-      install_requires = ["networkx"]
-)
-
+      install_requires=["networkx"],
+      )
